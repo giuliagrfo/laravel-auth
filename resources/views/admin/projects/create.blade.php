69f72a48e788d2aad031fccd535c91a7a858d234
giuliagrfo <giuliagrifo1@gmail.com>
2023-01-16 18:06:39 +0100

    Add Seeder

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -21,7 +21,7 @@
             <option selected>Select Type</option>
 
             @foreach ($types as $type )
-            <option value="{{$type->id}}" {{ old('type_id') ? 'selected' : '' }}>{{$type->name}}</option>
+            <option value="{{$type->id}}" {{ old('type_id') == $type->id ? 'selected' : '' }}>{{$type->name}}</option>
             @endforeach
 
         </select>
